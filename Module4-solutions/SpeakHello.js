a08e39bee2cf780ae4dbe66e621ad331339773ec
AANCHAL SENAPATI <49506280+Aanchalsenapati@users.noreply.github.com>
2019-05-09 22:52:53 -0400

    Update SpeakHello.js

--- Module4-solutions/SpeakHello.js
+++ Module4-solutions/SpeakHello.js
@@ -24,12 +24,10 @@
 // xxxx.xxxx = helloSpeaker;
 
 (function (window) {
-  var helloSpeaker = {};
-  var speakWord = "Hello";
-  helloSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-  }
-
-  window.helloSpeaker = helloSpeaker;
-
+	var helloSpeaker = {};
+	var speakWord = "Hello";
+helloSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker = helloSpeaker;
 })(window);
