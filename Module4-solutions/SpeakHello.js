a06fc8ce6be4540c781fbea71767c3420e9d3a15
AANCHAL SENAPATI <49506280+Aanchalsenapati@users.noreply.github.com>
2019-05-09 22:07:14 -0400

    Update SpeakHello.js

--- Module4-solutions/SpeakHello.js
+++ Module4-solutions/SpeakHello.js
@@ -22,3 +22,15 @@ function speak(name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+
+(function (window) {
+  var helloSpeaker = {};
+  helloSpeaker.name = "John";
+  var speakWord = "Hello ";
+  helloSpeaker.speak = function () {
+    console.log(speakWord + helloSpeaker.name + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
