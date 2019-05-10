421694ebcbf3b4e55ae253ba31abccf1456b56d1
AANCHAL SENAPATI <49506280+Aanchalsenapati@users.noreply.github.com>
2019-05-09 22:52:12 -0400

    Update SpeakGoodbye.js

--- Module4-solutions/SpeakGoodbye.js
+++ Module4-solutions/SpeakGoodbye.js
@@ -24,14 +24,11 @@
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
 
-
 (function (window) {
-  var byeSpeaker = {};
-  var speakWord = "Good Bye";
-  byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-  }
-
-  window.byeSpeaker = byeSpeaker;
-
+	var byeSpeaker = {};
+	var speakWord = "Good Bye";
+byeSpeaker.speak = function (name) {
+  console.log(speakWord + " " + name);
+}
+window.byeSpeaker = byeSpeaker;
 })(window);
