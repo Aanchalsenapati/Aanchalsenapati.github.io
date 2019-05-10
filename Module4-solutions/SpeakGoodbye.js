b77c9d165bc67a4f71c4d448d697bc989745f7df
AANCHAL SENAPATI <49506280+Aanchalsenapati@users.noreply.github.com>
2019-05-09 22:45:06 -0400

    Update SpeakGoodbye.js

--- Module4-solutions/SpeakGoodbye.js
+++ Module4-solutions/SpeakGoodbye.js
@@ -27,10 +27,9 @@
 
 (function (window) {
   var byeSpeaker = {};
-  byeSpeaker.names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   var speakWord = "Good Bye";
-  byeSpeaker.speak = function () {
-    console.log(speakWord + byeSpeaker.names + name);
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
   }
 
   window.byeSpeaker = byeSpeaker;
